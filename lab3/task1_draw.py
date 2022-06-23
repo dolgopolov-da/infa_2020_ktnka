d3d1c453f08ad293b7dcd2d54261575debf1a356
ktnka <dolgopolov_da@mail.ru>
2022-06-23 18:21:41 +0300

    Реализовал функцию main Реализовал функцию draw_angry_smile реализовал функцию draw_face Реализовал функции draw_left_eye и draw_right_eye
    Реализовал функции: draw_left_eyebrow, draw_right_eyebrow, draw_mouth
    Программа завершена, работает

--- lab3/task1_draw.py
+++ lab3/task1_draw.py
@@ -8,20 +8,47 @@ screen_height = 500
 screen_width = 500
 screen = pygame.display.set_mode((screen_height, screen_width))
 
+x, y = screen_height / 2, screen_width / 2
+R = screen_height / 4
+
+R_left_eye_max = R / 6
+R_left_eye_min = R / 12
+x_left_eye = x - 50
+y_left_eye = y - 40
+
+R_right_eye_max = R / 8
+R_right_eye_min = R / 14
+x_right_eye = x + 50
+y_right_eye = y - 40
+
+x_left_eyebrow = x_left_eye + R_left_eye_max * 0.7
+y_left_eyebrow = y_left_eye - R_left_eye_max * 0.9
+
+x_right_eyebrow = x_right_eye - R_left_eye_max * 0.7
+y_right_eyebrow = y_right_eye - R_left_eye_max * 0.8
+
+x_mouth = x
+y_mouth = y + R * 0.6
+'''
+y_left_eyebrow = y_left_eye - ((R_left_eye_max ** 2 - x_left_eyebrow ** 2) ** 0.5)
+x_start_pos_left_eyebrow = x_left_eyebrow + R_left_eye_max
+y_start_pos_left_eyebrow = y_left_eyebrow + R_left_eye_max
+x_end_pos_left_eyebrow = x_left_eyebrow - 2*R_left_eye_max
+y_end_pos_left_eyebrow = y_left_eyebrow - 2*R_left_eye_max
+start_pos_left_eyebrow = (x_start_pos_left_eyebrow, y_start_pos_left_eyebrow)
+end_pos_left_eyebrow = (x_end_pos_left_eyebrow, y_end_pos_left_eyebrow)
+'''
+
+
+
 def main():
-    x, y = screen_height/2, screen_width/2
-    R = screen_height/4
-
-    draw_angry_smile(x, y, R)
-
-
-def draw_angry_smile(x, y, R):
     draw_face(x, y, R)
-    draw_left_eye(x, y, R)
-    draw_right_eye(x, y, R)
-    draw_left_eyebrow()
-    draw_right_eyebrow()
-    draw_mouth()
+    draw_left_eye(x_left_eye, y_left_eye, R_left_eye_max, R_left_eye_min)
+    draw_right_eye(x_right_eye, y_right_eye, R_right_eye_max, R_right_eye_min)
+    draw_left_eyebrow(x_left_eyebrow, y_left_eyebrow)
+    #draw_left_eyebrow(x_left_eyebrow, y_left_eyebrow, x_start_pos_left_eyebrow, y_start_pos_left_eyebrow, x_end_pos_left_eyebrow, y_end_pos_left_eyebrow)
+    draw_right_eyebrow(x_right_eyebrow, y_right_eyebrow)
+    draw_mouth(x_mouth, y_mouth)
 
 
 def draw_face(x, y, R):
@@ -30,36 +57,30 @@ def draw_face(x, y, R):
     circle(screen, "black", (x, y), R, 2)
 
 
-def draw_left_eye(x, y, R):
-    R_left_eye_max = R / 6
-    R_left_eye_min = R / 12
-    x_left_eye = x - 50
-    y_left_eye = y - 40
+def draw_left_eye(x_left_eye, y_left_eye, R_left_eye_max, R_left_eye_min):
     circle(screen, "red", (x_left_eye, y_left_eye), R_left_eye_max)
     circle(screen, "black", (x_left_eye, y_left_eye), R_left_eye_min)
     circle(screen, "black", (x_left_eye, y_left_eye), R_left_eye_max, 1)
 
 
-def draw_right_eye(x, y, R):
-    R_right_eye_max = R / 8
-    R_right_eye_min = R / 14
-    x_right_eye = x + 50
-    y_right_eye = y - 40
+def draw_right_eye(x_right_eye, y_right_eye, R_right_eye_max, R_right_eye_min):
     circle(screen, "red", (x_right_eye, y_right_eye), R_right_eye_max)
     circle(screen, "black", (x_right_eye, y_right_eye), R_right_eye_min)
     circle(screen, "black", (x_right_eye, y_right_eye), R_right_eye_max, 1)
 
 
-def draw_left_eyebrow():
-    pass
+def draw_left_eyebrow(x_left_eyebrow, y_left_eyebrow):
+    line(screen, "black", [x_left_eyebrow + 15, y_left_eyebrow + 10],
+         [x_left_eyebrow - 60, y_left_eyebrow - 40], 10)
 
 
-def draw_right_eyebrow():
-    pass
+def draw_right_eyebrow(x_right_eyebrow, y_right_eyebrow):
+    line(screen, "black", [x_right_eyebrow - 15, y_right_eyebrow + 5],
+         [x_right_eyebrow + 75, y_right_eyebrow - 25], 10)
 
 
-def draw_mouth():
-    pass
+def draw_mouth(x_mouth, y_mouth):
+    line(screen, "black", (x_mouth - R/2, y_mouth), (x_mouth + R/2, y_mouth), 15)
 
 
 main()
