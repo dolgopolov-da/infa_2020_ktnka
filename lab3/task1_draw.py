77fffc652df26b3d3217749290e5731a44d49f03
ktnka <dolgopolov_da@mail.ru>
2022-06-23 18:28:08 +0300

    Программа завершена, работает Почистил от лишнего

--- lab3/task1_draw.py
+++ lab3/task1_draw.py
@@ -32,6 +32,7 @@ y_mouth = y + R * 0.6
 
 
 def main():
+    '''Рисует смайлик целиком'''
     draw_face(x, y, R)
     draw_left_eye(x_left_eye, y_left_eye, R_left_eye_max, R_left_eye_min)
     draw_right_eye(x_right_eye, y_right_eye, R_right_eye_max, R_right_eye_min)
@@ -41,34 +42,40 @@ def main():
 
 
 def draw_face(x, y, R):
+    '''Рисует окружность лица'''
     rect(screen, "gray", (0, 0, screen_height, screen_width))
     circle(screen, "yellow", (x, y), R)
     circle(screen, "black", (x, y), R, 2)
 
 
 def draw_left_eye(x_left_eye, y_left_eye, R_left_eye_max, R_left_eye_min):
+    '''Рисует левый глаз'''
     circle(screen, "red", (x_left_eye, y_left_eye), R_left_eye_max)
     circle(screen, "black", (x_left_eye, y_left_eye), R_left_eye_min)
     circle(screen, "black", (x_left_eye, y_left_eye), R_left_eye_max, 1)
 
 
 def draw_right_eye(x_right_eye, y_right_eye, R_right_eye_max, R_right_eye_min):
+    '''Рисует правый глаз'''
     circle(screen, "red", (x_right_eye, y_right_eye), R_right_eye_max)
     circle(screen, "black", (x_right_eye, y_right_eye), R_right_eye_min)
     circle(screen, "black", (x_right_eye, y_right_eye), R_right_eye_max, 1)
 
 
 def draw_left_eyebrow(x_left_eyebrow, y_left_eyebrow):
+    '''Рисует левую бровь'''
     line(screen, "black", [x_left_eyebrow + 15, y_left_eyebrow + 10],
          [x_left_eyebrow - 60, y_left_eyebrow - 40], 10)
 
 
 def draw_right_eyebrow(x_right_eyebrow, y_right_eyebrow):
+    '''Рисует правую бровь'''
     line(screen, "black", [x_right_eyebrow - 15, y_right_eyebrow + 5],
          [x_right_eyebrow + 75, y_right_eyebrow - 25], 10)
 
 
 def draw_mouth(x_mouth, y_mouth):
+    '''Рисует рот'''
     line(screen, "black", (x_mouth - R/2, y_mouth), (x_mouth + R/2, y_mouth), 15)
 
 
