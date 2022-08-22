6dbd6dcc36fae41e23f20910fd4e5516a6e1f141
ktnka <dolgopolov_da@mail.ru>
2022-08-22 17:03:20 +0300

    Job done!

--- stepic/python_newbie/7.9_cifrovoy_koren.py
+++ stepic/python_newbie/7.9_cifrovoy_koren.py
@@ -10,31 +10,14 @@
 
 n = int(input())
 total = 0
-subtotal = 0
-subsubtotal = 0
 
 while n > 9:
-    total = 0
-
-    while n > 0:
+    while n != 0:
         last_num = n % 10
         total += last_num
         n //= 10
-    if total // 10 == 0:
-        print(total)
-        break
-    while total != 0:
-        last_num = total % 10
-        subtotal += last_num
-        total //= 10
-    if subtotal // 10 == 0:
-        print(subtotal)
-        break
-    while subtotal != 0:
-        last_num = subtotal % 10
-        subsubtotal += last_num
-        subtotal //= 10
-    print(subsubtotal)
+    n, total = total, 0
 
+print(n)
 
 
