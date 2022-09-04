32006a44f79e82b30ad51947c488e778db5faf78
ktnka <dolgopolov_da@mail.ru>
2022-09-04 13:28:56 +0300

    Job done! Добавлен счетчик попыток, добавлена возможность повторить игру Добавлена возможность выбрать предел загадываемого числа

--- stepic/python_newbie/numerical_guessing.py
+++ stepic/python_newbie/numerical_guessing.py
@@ -1,24 +1,44 @@
 import random
 
-guess_number = random.randint(1, 100)
-print('Добро пожаловать в числовую угадайку')
 
-def is_valid(text):
+
+def is_valid(text, n):
     for i in range(len(text)):
         if not text[i].isdigit():
             return False
-    if not int(text) in range(1, 101):
+    if not int(text) in range(1, n):
         return False
     else:
         return True
 
 def main():
+    while True:
+        n = input('Введите максимальный предел загадываемого числа (целое число больше 1): ')
+        is_n_digit = True
+        for i in range(len(n)):
+            if not n[i].isdigit():
+                print('Нужно ввести целое число больше 1.')
+                is_n_digit = False
+                break
+        if not is_n_digit:
+            continue
+        else:
+            if int(n) > 1 and float(n) == int(n):
+                n = int(n)
+                break
+            else:
+                print('Нужно ввести целое число больше 1.')
+
+    guess_number = random.randint(1, n)
+    print('Добро пожаловать в числовую угадайку')
+
     game = True
     count = 0
+
     while game:
         count += 1
-        txt = input('Введите число от 1 до 100: ')
-        if not is_valid(txt):
+        txt = input(f'Введите число от 1 до {n}: ')
+        if not is_valid(txt, n):
             print('А может быть все-таки введем целое число от 1 до 100?')
             continue
         else:
